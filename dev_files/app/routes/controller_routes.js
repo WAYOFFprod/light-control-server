4d1aa6f5c1f3715c01776b881a48d54f1306e6fe
Raphael-macbook <raphael@wayoff.ch>
2018-06-15 13:40:18 +0200

    update...

--- dev_files/app/routes/controller_routes.js
+++ dev_files/app/routes/controller_routes.js
@@ -1,11 +1,24 @@
 import express from 'express';
-const router = express.Router();
+
+import controllerModel from '../models/controller';
 import axios from '../../config/axios';
+
+const router = express.Router();
 
 
 var controllers = []
 router.post('/connect', (req, res) => {
   console.log("connect: ", req.body);
+  var controller = {
+    id: req.body.id,
+    name: req.body.name,
+    controller_type: req.body.controller_type,
+    controller_model: req.body.controller_model,
+    status: 'on',
+    ip: req.body.ip
+  }
+  var controllerData = controllerModel (controller);
+  controllerData.save();
   res.send(req.body.type + ' connected to the server')
 });
 
--- dev_files/server.js
+++ dev_files/server.js
@@ -8,13 +8,12 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-const db = mongoose.connect(dbconf.localUrl);
+const db = mongoose.connect(dbconf.localUrl, {userMongoClient: true});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 app.get('/', (req, res) => res.send('Welcome to the light control interface!!!'))
-//require('./app/routes')(app, db);
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
